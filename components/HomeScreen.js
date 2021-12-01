5e9d706fbaa6cccbe6f9c560f7f62a50a50cbeec
Sanjit Juneja <sanjit.j55@gmail.com>
2021-11-30 21:15:15 -0600

    added viewAttendees, fixed Profile page, Home page

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -53,22 +53,7 @@ function HomeScreen({ navigation }) {
             style={styles.flatlist}
             data={data}
             renderItem={({ item }) => (
-              <TouchableOpacity
-                style={styles.listItem}
-                onPress={() => {
-                  console.log(item.sport);
-                  navigation.navigate("viewEvent", {
-                    sport: item.sport,
-                    when: item.when,
-                    where: item.where,
-                    location: item.location,
-                    description: item.description,
-                    group: item.group,
-                    groupSize: item.groupSize,
-                    users: item.users,
-                  });
-                }}
-              >
+              <TouchableOpacity style={styles.listItem}>
                 <View
                   style={{
                     flexDirection: "row",
@@ -94,8 +79,53 @@ function HomeScreen({ navigation }) {
     );
   } else {
     return (
-      <View>
-        <Text>Hello</Text>
+      <View style={styles.container}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.liveButton}
+            onPress={() => {
+              setLive(true);
+            }}
+          >
+            <Text style={styles.buttonText}>Live</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.scheduleButton}
+            onPress={() => {
+              setLive(false);
+            }}
+          >
+            <Text style={styles.buttonText}>Scheduled</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.list}>
+          <Text style={styles.titleStyle}>Scheduled Events</Text>
+          <FlatList
+            style={styles.flatlist}
+            data={data}
+            renderItem={({ item }) => (
+              <TouchableOpacity style={styles.listItem}>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                  }}
+                >
+                  <Image
+                    source={require("../assets/soccer.png")}
+                    style={{ width: 75, height: 75, marginRight: 10 }}
+                  />
+                  <View style={{ marginLeft: "15%" }}>
+                    <Text style={styles.textStyle}>{item.sport}</Text>
+                    <Text style={styles.textStyle}>{item.when}</Text>
+                    <Text style={styles.textStyle}>{item.where}</Text>
+                  </View>
+                </View>
+              </TouchableOpacity>
+            )}
+          />
+        </View>
       </View>
     );
   }
---
+++ database/firebaseDb.js
@@ -0,0 +1,18 @@
+import * as firebase from 'firebase';
+import firestore from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCeQj8YIOe-Ebg0ZNZTiKYNQRPmQPEGlE4",
+    authDomain: "convergent-health.firebaseapp.com",
+    databaseURL: "https://convergent-health-default-rtdb.firebaseio.com",
+    projectId: "convergent-health",
+    storageBucket: "convergent-health.appspot.com",
+    messagingSenderId: "791844457283",
+    appId: "1:791844457283:web:454fe40c9cacfa0977bdf6",
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+firebase.firestore();
+
+export default firebase;
